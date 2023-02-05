5acd1ce167ed945dbde52967bb1be525aaf9661e
SilentCruzer <radithyakumar7@gmail.com>
2023-02-05 13:34:00 +0530

    Adds deploy script

--- hardhat-files/contracts/Whitelist.sol
+++ hardhat-files/contracts/Whitelist.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract WhiteList {
+contract Whitelist {
     uint8 public maxWhitelistedAddresses;
 
     mapping(address => bool) public whitelistedAddresses;
