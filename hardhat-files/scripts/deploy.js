5462770e875e6e400b42d134d627bd05e2dae21e
SilentCruzer <radithyakumar7@gmail.com>
2023-02-05 14:15:00 +0530

    Adds base project for frontend

--- hardhat-files/scripts/deploy.js
+++ hardhat-files/scripts/deploy.js
@@ -1,5 +1,5 @@
 const { ethers } = require("hardhat");
-
+// Whitelist Contract Address: 0xb4d11574405a9BC83937AD1AB2742f9caA0d1153
 async function main() {
   const whitelistContract = await ethers.getContractFactory("Whitelist");
   const deployedWhitelistContract = await whitelistContract.deploy(10);
